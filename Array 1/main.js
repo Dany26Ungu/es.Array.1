c15de60402b995f0f4a234511ac0546843eb64a0
Dany26Ungu <danielungureanu2610@gmail.com>
2026-02-25 12:59:25 +0100

    Add files via upload

---
+++ Array 1/main.js
@@ -0,0 +1,11 @@
+let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
+let sortedNumbers = numbers.sort((a, b)=> a - b);
+console.log(sortedNumbers);
+
+let sortedNumbers2 = sortedNumbers.reverse();
+console.log(sortedNumbers2);
+
+
+
+
+
